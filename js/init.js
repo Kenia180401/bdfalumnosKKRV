2da7ad4f3fefc6fcbbc23a4df8a04f1cbe5dfe26
icgrupo41 <87824480+icgrupo41@users.noreply.github.com>
2021-07-22 11:58:03 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBvqk0gH6mFFvHNRNlHfKaWwLPX62Uy_GI",
+    authDomain: "bfalumnos41.firebaseapp.com",
+    projectId: "bfalumnos41",
+    storageBucket: "bfalumnos41.appspot.com",
+    messagingSenderId: "977302937922",
+    appId: "1:977302937922:web:0694536fb9f3abaec0a8db",
+    measurementId: "G-4T0PFM1G87"
 });
